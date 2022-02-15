fd1c2164577aaf2ea30f5f4c6a51ec62cc8f9261
LZJ <lizj@fundtp.com>
2022-02-15 14:25:49 +0800

    remove firebase

--- src/components/domain/auth/SignInButton.tsx
+++ src/components/domain/auth/SignInButton.tsx
@@ -1,14 +1,13 @@
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
-import { useAuth } from "~/lib/firebase";
 
 export const SignInButton = () => {
   const handleClick = () => {
-    const provider = new GoogleAuthProvider();
-    const auth = useAuth();
-    // @see https://firebase.google.com/docs/auth/web/google-signin
-    auth.languageCode = "ja";
+    // const provider = new GoogleAuthProvider();
+    // const auth = useAuth();
+    // // @see https://firebase.google.com/docs/auth/web/google-signin
+    // auth.languageCode = "ja";
 
-    signInWithRedirect(auth, provider);
+    // signInWithRedirect(auth, provider);
   };
 
   return (
